b423b922c388f3f85fed047044358c8df554687e
syxxcalypso <jen.sanchez1404@gmail.com>
2021-02-13 23:58:58 -0700

    Fixed/Addedcomments

--- myshell.py
+++ myshell.py
@@ -5,11 +5,11 @@ import sys
 buf = ''
 _next = 0
 limit = 0
-max_bytes = 4
+max_bytes = 1000
 eof = ''
 
 def getchar():
-    global eof
+    global eof                                  # Access globals
     global buf
     global _next
     global limit
@@ -18,69 +18,67 @@ def getchar():
         buf  += os.read(0, max_bytes).decode()  # Fill / add to buffer
         limit = len(buf)                        # Get size of buffer
 
-        if limit == 0: # If read no chars
+        if limit == 0:                          # If read no chars
             return eof
 
-    c = buf[_next] # Get char
-    _next += 1     # Setup for next char
-    return c       # Return the char
+    c = buf[_next]                              # Get char
+    _next += 1                                  # Setup for next char
+    return c                                    # Return the char
 
 def readline():
 
-    # Access globals
-    global buf
+    global buf                                  # Access globals
     global _next
     global limit
     global eof
 
-    c = getchar()                   # Get first char
-    while c != '\n' and c != eof:   # While char is valid
-        c = getchar()               # get next char
+    c = getchar()                               # Get first char
+    while c != '\n' and c != eof:               # While char is valid
+        c = getchar()                           # get next char
 
-    tmp = buf[:_next] # get all chars up to newline (including newline)
-    buf = buf[_next:] # clear buffer upto newline
-    limit = len(buf)  # reset limit
-    _next = 0         # reset next
-    return tmp        # return the line
+    tmp = buf[:_next]                           # get all chars up to newline (including newline)
+    buf = buf[_next:]                           # clear buffer upto newline
+    limit = len(buf)                            # reset limit
+    _next = 0                                   # reset next
+    return tmp                                  # return the line
 
 
+def shell_loop(prompt='$ '.encode()):           # function for shell loop
+    global eof                                  # get global eof
 
-def shell_loop(prompt='$ '.encode()):
-    global eof # get global eof
+    os.write(1, prompt)                         # Prompt
+    sys.stdout.flush()                          # flush since no '/n'
+    line = readline()                           # Get first line
+    while line != eof and line != 'exit\n':     # While line is valid
+        if line != '\n':                        # if line is empty
+            line = line[:-1]                    # Cut off the newline char
+            run(tokenize(line))                 # executes with the list returned from tokenize()
+        os.write(1, prompt)                     # Prompt
+        sys.stdout.flush()                      # flush to guarantee write
+        line = readline()                       # get next line
 
-    os.write(1, prompt)       # Prompt
-    sys.stdout.flush()
-    line = readline()         # Get first line
-    while line != eof and line != 'exit\n':        # While line is valid
-        if line != '\n':      # if line is empty
-            line = line[:-1]   # Cut off the newline char
-            run(tokenize(line))    # executes with the list returned from tokenize()
-        os.write(1, prompt)   # Prompt
-        sys.stdout.flush()
-        line = readline()  # get next line
+def tokenize(line):                             # tokenize function
+    return line.split(' ')                      # returns tokenized string
 
-def tokenize(line):
-    return line.split(' ')
-
-def run(tokens):
-    proc_id = os.fork()
-    if proc_id < 0:
-        os.write(2, "Fork has failed".encode())
+def run(tokens):                                # run command
+    proc_id = os.fork()                         # starts new child shell
+    if proc_id < 0:                             # check if valid fork
+        os.write(2, "Fork has failed".encode()) # error message to STDERR
         sys.exit(1)
     elif proc_id == 0:
-        path = get_env(tokens)
-        if path:
-            os.execve(path, tokens, os.environ)
+        path = get_env(tokens)                  # path set to string path
+        if path:                                # true = valid
+            os.execve(path, tokens, os.environ) # execute command
     else:
-        os.wait()
+        os.wait()                               # wait for child to terminate
         return
 
-def get_env(tokens):
-    path_list = os.environ["PATH"].split(":")
-    for path in path_list:
+def get_env(tokens):                            # get envir path
+    path_list = os.environ["PATH"].split(":")   # removes returned : to get valid path to return
+    for path in path_list:                      # checks for path in the listdir()
         if tokens[0] in os.listdir(path):
-            return path + '/' + tokens[0]
-    print("command not found " + tokens[0])
+            return path + '/' + tokens[0]       # path found, return with valid syntax
+    print("command not found " + tokens[0])     # prints error msg
     return False
     
 
